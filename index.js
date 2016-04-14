255a61eb5eaf0ea6527c731073621a3bd7a7107c
Woo Yun <hh747hh@gmail.com>
2016-04-14 13:53:52 -0400

    handlebars installed, layout-main, app-welcome, index added, index view shows

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-var express = require("express");
-
+var express   = require("express");
+var hbs       = require("express-handlebars");
+var mongoose  = require("./db/connection");
 
 var app = express();
 
+var Exercise = mongoose.model("Exercise");
+
+app.set("port", process.env.PORT || 3001);
+app.set("view engine", "hbs");
+app.engine(".hbs", hbs({
+  extname:    ".hbs",
+  partialsDir: "views/",
+  layoutsDir:   "views/",
+  defaultLayout:  "layout-main"
+}));
+app.use("/assets", express.static("public"));
+
+
+
 app.get("/", function(req, res){
-  console.log("Hello");
+  res.render("app-welcome");
+});
+
+app.get("/exercises", function(req, res){
+  Exercise.find({}.then(function(exercises){
+    res.render("index", {
+      exercises: exercises
+    });
+  }));
 });
 
 
 
-
-
-app.listen(3001, function(){
+app.listen(app.get("port"), function(){
   console.log("I am here!");
 });
